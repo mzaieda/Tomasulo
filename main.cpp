898447854c0538129db91e9a577f85ab53f4f02b
mzaieda <53624383+mzaieda@users.noreply.github.com>
2020-12-02 23:40:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,58 @@
 #include<vector>
 using namespace std;
 
+class ReservationStation{
+private:
+	string name;
+	bool busy;
+	string op;
+	string Vj;
+	string Vk;
+	string Qj;
+	string Qk;
+	long addr;
+public:
+	string setName(string nam){
+		name = nam;
+	}
+	bool setBusy(bool bus){
+		busy = bus;
+	}
+	string setOp(string Op){
+		op = Op;
+	}
+	string setVj(string vj){
+		Vj = vj;
+	}
+	string setVk(string vk){
+		Vk = vk;
+	}
+	string setQj(string qj){
+		Qj = qj;
+	}
+	string setQk(string qk){
+		Qk = qk;
+	}
+	long setAddr(string add){
+		addr = add;
+	}
+	void getName(){return name;}
+	void getBusy(){return busy;}
+	void getOp(){return op;}
+	void getVj(){return Vj;}
+	void getVk(){return Vk;}
+	void getQj(){return Qj;}
+	void getQk(){return Qk;}
+	void getAddr(){return addr;}
+	
+};
+
+class RegisterStat{
+private:
+	vector<pair<string, string> > regStat;
+public:
+	
+};
 
 int main(int argC, char **argv) {
 /*
